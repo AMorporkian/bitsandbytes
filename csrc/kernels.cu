fc362df196a55ffefcd0dac2a3800596ebb1cfc1
SargeZT <patrick.e.shanahan@gmail.com>
2023-06-02 08:46:28 -0500

    test?

--- csrc/kernels.cu
+++ csrc/kernels.cu
@@ -946,6 +946,8 @@ __global__ void kPreconditionOptimizer32bit2State(T* g, T* p,
   for (unsigned int i = base_idx; i < n_full; i += gridDim.x*BLOCK_SIZE)
   {
       valid_items = n - i >= (BLOCK_SIZE) ? (BLOCK_SIZE) : n - i;
+  }
+
 template<typename T, int OPTIMIZER, int BLOCK_SIZE, int NUM_VALS>
 __launch_bounds__(BLOCK_SIZE/NUM_VALS, 1)
 __global__ void kPreconditionOptimizer32bit2State(T* g, T* p,
